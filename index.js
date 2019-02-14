6d256308da965e8c1d48e4c6d42a5f996957594a
David Cantoran <davidcantoran36@gmail.com>
2019-02-14 22:48:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, value, key) {
-  return Object.assign({}, object, [key]: value);
+  return Object.assign({}, object, { [key]: value) };
   
 }
