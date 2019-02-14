aa9b5f7a5de50b58543b40496c21c0ea127ddcac
David Cantoran <davidcantoran36@gmail.com>
2019-02-14 23:02:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+
+
+
+
